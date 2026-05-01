bda95a0742ce31d844a5dae702420c3dddc1bb6b
Nata <rybapoint@gmail.com>
2026-05-01 15:57:32 +0300

    Stimulus

---
+++ assets/bootstrap.js
@@ -0,0 +1,20 @@
+import { startStimulusApp } from '@symfony/stimulus-bridge';
+
+
+// Registers Stimulus controllers from controllers.json and in the controllers/ directory
+// export const app = startStimulusApp(require.context(
+//     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
+//     true,
+//     /\.[jt]sx?$/
+// ));
+// register any custom, 3rd party controllers here
+// app.register('some_controller_name', SomeImportedController);
+
+
+
+// автоматически загружает все контроллеры из controllers/
+export const app = startStimulusApp(require.context(
+    './controllers',
+    true,
+    /\.js$/
+));
--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -7,18 +7,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Psr\Log\LoggerInterface;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
-    public function getSong(int $id): Response
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'], name: 'api_songs_get_one')]
+    public function getSong(int $id, LoggerInterface  $logger): Response
     {
         $song = [
             'id' => $id,
             'name' => 'Creep',
-            'url' => 'tiscom',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
 
+$logger->info("Returning API response for song {song} :   ".json_encode($song), ['song' => $id]);
 
         return $this->json($song);
     }
